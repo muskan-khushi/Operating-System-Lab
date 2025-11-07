963f006d4f1153eebb0e7cb9c6465a6f314b59a7
Muskan <muskankhushi2022@gmail.com>
2025-11-07 15:49:59 +0530

    .

--- schedulingAlgo.cpp
+++ schedulingAlgo.cpp
@@ -19,117 +19,27 @@ int main(){
         p[i].rem_bt = p[i].bt;
     }
     
-    // //First Come First Serve Algo Logic 
-    // for (int i=0; i<n-1; i++){
-    //     for (int j=i+1; j<n; j++){
-    //         if (p[i].at > p[j].at){
-    //             struct Process temp = p[i];
-    //             p[i] = p[j];
-    //             p[j] = temp;
-    //         }
-    //     }
-    // }
-    
-    // time = 0;
-    // for(int i=0; i<n; i++){
-    //     if (time < p[i].at) time = p[i].at;
-    //     time += p[i].bt;
-    //     p[i].ct = time;
-    //     p[i].tat = p[i].ct - p[i].at;
-    //     p[i].wt = p[i].tat - p[i].bt;
-    // }
-    
-    //-----------------------------------------------------
-    
-    // //Shortest Job First Scheduling Algorithm
-    // time = 0;
-    // completed = 0;
-    
-    // while (completed != n){
-    //     int min = 9999, pos = -1;
-        
-    //     for (int i=0; i<n; i++){
-    //         if (p[i].at <= time && p[i].bt < min && p[i].ct == 0){
-    //             min = p[i].bt;
-    //             pos = i
-    //         }
-    //     }
-        
-    //     if (pos == -1){
-    //         time++; 
-    //         continue;
-    //     }
-        
-    //     time += p[pos].bt;
-    //     p[pos].ct = time;
-    //     p[pos].tat = p[pos].ct - p[pos].at;
-    //     p[pos].wt = p[pos].tat - p[pos].bt;
-    //     completed++;
-    //}
-    
-    //-------------------------------------------------------
-    
-    //Shortest Job Remaining First Algorithm
-    //min -> min_rem and bt -> rem_bt
-    // int prev = -1;
-    // while (completed != n){
-    //     int min_rem = 9999;
-    //     int pos = -1;
-        
-    //     for (int i=0; i<n; i++){
-    //         if (p[i].at <= time && p[i].rem_bt < min_rem && p[i].rem_bt > 0 ){
-    //             min_rem = p[i].rem_bt;
-    //             pos = i;
-    //         }
-    //     }
-        
-    //     if (pos == -1) {
-    //         time++;
-    //         continue;
-    //     }
-        
-    //     p[pos].rem_bt--;
-    //     time++;
-        
-    //     if (p[pos].rem_bt == 0){
-    //         completed++;
-    //         p[pos].ct = time;
-    //         p[pos].tat = p[pos].ct - p[pos].at;
-    //         p[pos].wt = p[pos].tat - p[pos].bt;
-    //     }
-    // }
-    
-    //--------------------------------------------------
-
-    //Round Robin Scheduling Algorithm
-    printf("Enter time quantum: ");
-    scanf("%d", &tq);
-
-    int rem = n;
-    time = 0;
-
-    while (rem > 0){
-        int done_in_cycle = 0;
-        for (int i=0; i<n; i++){
-            if (p[i].at <= time && p[i].rem_bt > 0){
-                done_in_cycle = 1;
-
-                if (p[i].rem_bt > tq){
-                    time += tq;
-                    p[i].rem_bt -= tq;
-                }
-                else {
-                    time += p[i].rem_bt;
-                    p[i].rem_bt = 0;
-                    p[i].ct = time;
-                    p[i].tat = p[i].ct - p[i].at;
-                    p[i].wt = p[i].tat - p[i].bt;
-                    rem--;
-                }
+    //First Come First Serve Algo Logic 
+    for (int i=0; i<n-1; i++){
+        for (int j=i+1; j<n; j++){
+            if (p[i].at > p[j].at){
+                struct Process temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
             }
         }
-        if (!done_in_cycle) time++;
     }
+    
+    time = 0;
+    for(int i=0; i<n; i++){
+        if (time < p[i].at) time = p[i].at;
+        time += p[i].bt;
+        p[i].ct = time;
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+    }
+    
+    
     
     
     
